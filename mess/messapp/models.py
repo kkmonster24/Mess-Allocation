7287868630b9164a8e4214bd45d6d8246ebdba33
amritsaha607 <41221083+amritsaha607@users.noreply.github.com>
2019-05-21 18:14:26 +0530

    Add files via upload

--- mess/messapp/models.py
+++ mess/messapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
 
@@ -15,7 +16,22 @@ class Hostel(models.Model):
     
     def __str__(self):
         return self.name
+    
 
+class Feedback(models.Model):
+    uniform_and_punctuality = models.IntegerField()
+    cleanliness_and_hygiene = models.IntegerField()
+    waste_disposal = models.IntegerField()
+    quality_of_ingredients = models.IntegerField()
+    overall_satisfaction_of_breakfast = models.IntegerField()
+    overall_satisfaction_of_lunch = models.IntegerField()
+    overall_satisfaction_of_dinner = models.IntegerField()
+    create_time = models.DateTimeField(default=datetime.now)
+
+    def __str__(self):
+        opi = (self.uniform_and_punctuality + self.cleanliness_and_hygiene + self.waste_disposal + 2*self.quality_of_ingredients + 3*self.overall_satisfaction_of_breakfast + 3*self.overall_satisfaction_of_lunch + 3*self.overall_satisfaction_of_dinner)/(1+1+1+2+3+3+3)
+        return str(opi)
+        
     
 class Student(models.Model):
     first_name = models.CharField(max_length=50)
@@ -24,7 +40,8 @@ class Student(models.Model):
     password = models.CharField(max_length=30)
     roll = models.IntegerField()
     subscribed_mess_hostel = models.ForeignKey(Hostel, on_delete=models.CASCADE, related_name='choice_students')
-    feedback = models.IntegerField(blank=True, null=True)
+    #feedback = models.IntegerField(blank=True, null=True)
+    feedback = models.OneToOneField(Feedback, on_delete=models.CASCADE, related_name='choice_student', null=True, blank=True)
     feedback_given = models.BooleanField(default=False)
     mess_off_dates = models.ManyToManyField(Date, related_name='choice_students', blank=True)
     authent = models.BooleanField(default=False)
--- mess/messapp/views.py
+++ mess/messapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import *
+from .models import *
+from datetime import datetime
 
 # Create your views here.
 
@@ -65,6 +67,62 @@ def feedback(request):
     if 'user_id' in request.session:
         user = Student.objects.get(id=request.session['user_id'])
         if user.authent:
+            # check if feedback is done for this month or not
+            if user.feedback.create_time.month == datetime.now().month:
+                print('wow !! Feedback done for this month !!')
+            else:
+                # month changed, so delete the previous feedback object and make the student's feedbck field empty, turn the feedback_given field off
+                f = user.feedback
+                f.delete()
+                
+                user.feedback_given = False
+                user.save()
+                
+                if user.feedback:
+                    print('not deleted\n\n')
+                else:
+                    print('deleted\n')
+                print('nooooppsyyyyyyy, we need to delete prev model')
+            if user.feedback:
+                # check if mnonth has expired
+                print('\nfeedback creation time : ' + str(user.feedback.create_time) + '\n')
+                print('ohhhhhhh yeahhhhh!!')
+            else:
+                print('nooooooooooooooooouuuu!')
+            
+            if request.method=='POST':
+                print(request.POST)
+                
+                # check if he/she has already rated 
+                if user.feedback:
+                    f = user.feedback
+                    f.uniform_and_punctuality = request.POST['uniform_and_punctuality']
+                    f.cleanliness_and_hygiene = request.POST['cleanliness_and_hygiene']
+                    f.waste_disposal = request.POST['waste_disposal']
+                    f.quality_of_ingredients = request.POST['quality_of_ingredients']
+                    f.overall_satisfaction_of_breakfast = request.POST['overall_satisfaction_of_breakfast']
+                    f.overall_satisfaction_of_lunch = request.POST['overall_satisfaction_of_lunch']
+                    f.overall_satisfaction_of_dinner = request.POST['overall_satisfaction_of_dinner']
+                    f.save()
+                else:
+                    # create a new feedback object
+
+                    newFeed = Feedback(
+                        uniform_and_punctuality = request.POST['uniform_and_punctuality'],
+                        cleanliness_and_hygiene = request.POST['cleanliness_and_hygiene'],
+                        waste_disposal = request.POST['waste_disposal'],
+                        quality_of_ingredients = request.POST['quality_of_ingredients'],
+                        overall_satisfaction_of_breakfast = request.POST['overall_satisfaction_of_breakfast'],
+                        overall_satisfaction_of_lunch = request.POST['overall_satisfaction_of_lunch'],
+                        overall_satisfaction_of_dinner = request.POST['overall_satisfaction_of_dinner'],
+                    )
+                    newFeed.save()
+
+                    # connect the feedback object to the student
+
+                    user.feedback = newFeed
+                    user.save()
+
             return render(request, 'messapp/feedback.html', {'user': user})
         else:
             del request.session['user_id']
